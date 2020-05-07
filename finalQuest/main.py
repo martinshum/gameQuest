8d3429bab47fb6fdec1baf0043dbd9488925e914
martinshum <62357646+martinshum@users.noreply.github.com>
2020-05-07 14:52:20 -0700

    added images, score, hitpoints, and ammo, along with hitting enemies = score and ammo / will fix moving screen and add in more enemies

--- finalQuest/main.py
+++ finalQuest/main.py
@@ -2,23 +2,34 @@
 # Shmup game - part 1
 # Video link: https://www.youtube.com/watch?v=nGufy7weyGY
 # Player sprite and movement
-
 import pygame as pg
 from pygame.sprite import Sprite
 import random
+from os import path
 
 # screen dimensions and fps
 WIDTH = 480
 HEIGHT = 600
 FPS = 60
-
+# variables
+score = 0
+ammo = 100
 # define colors
 WHITE = (255, 255, 255)
+DARKBLUE = (39, 54, 77)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+game_dir = path.join(path.dirname(__file__))
+
+# load all images here - background, player, mobs, lazer
+background_image = pg.image.load(game_dir + "/img/bg.png")
+background_rect = background_image.get_rect()
+background_rect2 = background_image.get_rect()
+player_image = pg.image.load(game_dir + "/img/player.png")
+mob_image = pg.image.load(game_dir + "/img/mob.png")
 
 # initialize pygame...
 pg.init()
@@ -27,16 +38,28 @@ screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption("Space Crusaders")
 clock = pg.time.Clock()
 
+# utils
+font_name = pg.font.match_font('arial')
+
+def draw_text(surf, text, size, x, y):
+    font = pg.font.Font(font_name, size)
+    text_surface = font.render(text, True, WHITE)
+    text_rect = text_surface.get_rect()
+    text_rect.midtop = (int(x), int(y))
+    surf.blit(text_surface, text_rect)
+        
 class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = pg.Surface((50,40))
-        self.image.fill(GREEN)
+        self.image = pg.transform.scale(player_image, (50, 40))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
-        self.speedy = 0
+        self.speedy = 10
+        self.hitpoints = 100
+        self.ammo = ammo
     def update(self):
         self.speedx = 0
         self.speedy = 0
@@ -53,11 +76,21 @@ class Player(Sprite):
         #     self.speedy = 8
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        # adds ammo when enemies are hit with lazer
+        hits = pg.sprite.groupcollide(mobs, lazers, True, True)
+        if hits:
+            self.ammo += 10
+        # while running:
+        #     if self.ammo == 0:
+        #         running = False
 # this uses the lazer class to make this a function of the player class
     def pew(self):
-        lazer = Lazer(self.rect.centerx, self.rect.top)
-        all_sprites.add(lazer)
-        lazers.add(lazer)
+        if self.ammo > 0:
+            lazer = Lazer(self.rect.centerx, self.rect.top)
+            all_sprites.add(lazer)
+            lazers.add(lazer)
+            self.ammo -=1
+
 
 class Mob(Sprite):
     def __init__(self):
@@ -97,8 +130,8 @@ all_sprites = pg.sprite.Group()
 mobs = pg.sprite.Group()
 lazers = pg.sprite.Group()
 player = Player()
-lazer = Lazer(player.rect.x, player.rect.y)
-all_sprites.add(lazer)
+# lazer = Lazer(player.rect.x, player.rect.y)
+# all_sprites.add(lazer)
 # this summons the mobs and if you change the range you can add or subtract mods
 for i in range(0,8):
     mob = Mob()
@@ -120,13 +153,37 @@ while running:
     all_sprites.update()
     # this checks for collissions and when mobs and lazers hit both are gone
     hits = pg.sprite.groupcollide(mobs, lazers, True, True)
+    # adds a score of 10 for each enemy
+    if hits:
+        score += 10
+        
 # when a mob hits the player the if statement makes the game stop
     hits = pg.sprite.spritecollide(player, mobs, False)
     if hits:
         running = False
 
+    if len(mobs) == 0:
+        for i in range(0,8):
+            mob = Mob()
+            all_sprites.add(mob)
+            mobs.add(mob)
+
+        
+    background_rect2.y = background_rect.y - 600
+    background_rect.y+= player.speedy
+    background_rect2.y+= player.speedy
+
+    if background_rect2.y >- 0:
+        background_rect.y = background_rect.y -600
+
     # Draw
-    screen.fill(RED)
+    screen.fill(DARKBLUE)
+    screen.blit(background_image, background_rect)
+    screen.blit(background_image, background_rect2)
+    draw_text(screen, str(score), 24, WIDTH / 2, 10)
+    draw_text(screen, str(player.ammo), 24, WIDTH / 4, 10)
+    draw_text(screen, str(player.hitpoints), 16, player.rect.x, player.rect.y)
+
     all_sprites.draw(screen)
     pg.display.flip()
 
