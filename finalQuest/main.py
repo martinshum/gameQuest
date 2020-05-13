ced4c7a1d57879c6915d141e2a7e077f750669e3
martinshum <62357646+martinshum@users.noreply.github.com>
2020-05-13 13:42:23 -0700

    set up a freeze for when the shield is gone / only works on one mob currently

--- finalQuest/main.py
+++ finalQuest/main.py
@@ -30,6 +30,8 @@ background_rect = background_image.get_rect()
 background_rect2 = background_image.get_rect()
 player_image = pg.image.load(game_dir + "/img/player.png")
 mob_image = pg.image.load(game_dir + "/img/mob.png")
+freeze_image = pg.image.load(game_dir + "/img/freeze.png")
+spit_img = pg.image.load(path.join(game_dir + "/img/spit.png"))
 
 # initialize pygame...
 pg.init()
@@ -47,7 +49,19 @@ def draw_text(surf, text, size, x, y):
     text_rect = text_surface.get_rect()
     text_rect.midtop = (int(x), int(y))
     surf.blit(text_surface, text_rect)
-        
+
+
+def draw_shield_bar(surf, x, y, pct):
+    if pct < 0:
+        pct = 0
+    BAR_LENGTH = 100
+    BAR_HEIGHT = 10
+    fill = (pct / 100) * BAR_LENGTH
+    outline_rect = pg.Rect(x, y, BAR_LENGTH, BAR_HEIGHT)
+    fill_rect = pg.Rect(x, y, fill, BAR_HEIGHT)
+    pg.draw.rect(surf, GREEN, fill_rect)
+    pg.draw.rect(surf, WHITE, outline_rect, 2)
+
 class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self)
@@ -58,11 +72,12 @@ class Player(Sprite):
         self.rect.bottom = HEIGHT - 10
         self.speedx = 0
         self.speedy = 10
+        self.shield = 100
         self.hitpoints = 100
         self.ammo = ammo
     def update(self):
         self.speedx = 0
-        self.speedy = 0
+        # self.speedy = 0
         keystate = pg.key.get_pressed()
         if keystate[pg.K_w]:
             self.pew()
@@ -75,11 +90,7 @@ class Player(Sprite):
         # if keystate[pg.K_s]:
         #     self.speedy = 8
         self.rect.x += self.speedx
-        self.rect.y += self.speedy
-        # adds ammo when enemies are hit with lazer
-        hits = pg.sprite.groupcollide(mobs, lazers, True, True)
-        if hits:
-            self.ammo += 10
+        # self.rect.y += self.speedy
         # while running:
         #     if self.ammo == 0:
         #         running = False
@@ -95,18 +106,25 @@ class Player(Sprite):
 class Mob(Sprite):
     def __init__(self):
         Sprite.__init__(self)
-        self.image = pg.Surface((40,40))
-        self.image.fill(YELLOW)
+        self.image = pg.transform.scale(mob_image, (40, 40))
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(0, WIDTH-self.rect.width)
         self.rect.y = random.randrange(0, 240)
         self.speedx = random.randrange(1,10)
-        self.speedy = random.randrange(1,10)
+        self.speedy = random.randrange(0, 10)
+    def pew(self):
+        spit = Spit(self.rect.centerx, self.rect.top)
+        all_sprites.add(spit)
+        spits.add(spit)
+        # print('trying to shoot..')
     def update(self):
         self.rect.x += self.speedx
         if self.rect.x > WIDTH or self.rect.x < 0:
             self.speedx*=-1
             self.rect.y += 25
+        if random.random() > 0.99:
+            self.pew()
         if self.rect.y > HEIGHT:
             self.rect.y = -25
             self.rect.x = random.randrange(0, WIDTH-self.rect.width)
@@ -124,20 +142,36 @@ class Lazer(Sprite):
         self.rect.y += self.speedy
         if self.rect.bottom < 0:
             self.kill()
-
+class Spit(Sprite):
+    def __init__(self, x, y):
+        Sprite.__init__(self)
+        self.image = spit_img
+        # self.image = pg.Surface((5,10))
+        # self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = 10
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.y < 0:
+            self.kill()
+            # print(len(lazers))
 # where all the new things are created for the game
 all_sprites = pg.sprite.Group()
 mobs = pg.sprite.Group()
 lazers = pg.sprite.Group()
 player = Player()
+spits = pg.sprite.Group()
+all_sprites.add(player)
+freezes = pg.sprite.Group()
 # lazer = Lazer(player.rect.x, player.rect.y)
 # all_sprites.add(lazer)
 # this summons the mobs and if you change the range you can add or subtract mods
-for i in range(0,8):
+for i in range(0,3):
     mob = Mob()
     all_sprites.add(mob)
     mobs.add(mob)
-all_sprites.add(player)
 
 # the game loop
 running = True
@@ -153,10 +187,22 @@ while running:
     all_sprites.update()
     # this checks for collissions and when mobs and lazers hit both are gone
     hits = pg.sprite.groupcollide(mobs, lazers, True, True)
-    # adds a score of 10 for each enemy
+    # adds a score and ammo for each enemy killed
     if hits:
-        score += 10
-        
+        score += 100
+        player.ammo +=100
+    hits = pg.sprite.spritecollide(player, spits, False)
+
+    if hits:
+        player.shield -= 10
+
+    if player.shield == 0:
+        mob.speedx = 0
+
+
+
+
+
 # when a mob hits the player the if statement makes the game stop
     hits = pg.sprite.spritecollide(player, mobs, False)
     if hits:
@@ -168,10 +214,9 @@ while running:
             all_sprites.add(mob)
             mobs.add(mob)
 
-        
     background_rect2.y = background_rect.y - 600
-    background_rect.y+= player.speedy
-    background_rect2.y+= player.speedy
+    background_rect.y += player.speedy
+    background_rect2.y += player.speedy
 
     if background_rect2.y >- 0:
         background_rect.y = background_rect.y -600
@@ -183,7 +228,7 @@ while running:
     draw_text(screen, str(score), 24, WIDTH / 2, 10)
     draw_text(screen, str(player.ammo), 24, WIDTH / 4, 10)
     draw_text(screen, str(player.hitpoints), 16, player.rect.x, player.rect.y)
-
+    draw_shield_bar(screen, 5, 5, player.shield)
     all_sprites.draw(screen)
     pg.display.flip()
 
